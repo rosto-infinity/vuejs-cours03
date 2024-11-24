beed5edcfd2619cfc07e9468583a89a8ee47113b
ROSTAND LELE <rostoinfocus@gmail.com>
2024-11-24 23:22:30 +0100

    Checkbox
    Checkbox

---
+++ src/Checkbox.vue
@@ -0,0 +1,15 @@
+<template>
+  <ol class="list-disc">
+    <li v-for="color in colors">{{ color }}</li>
+  </ol>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+  const colors = ref([
+    "Red", 
+    "Green",
+     "Blue",
+      "Gray"
+  ]);
+</script>
