eeabdd9827049be7983387daa300f63996302efb
ROSTAND LELE <rostoinfocus@gmail.com>
2024-11-24 22:07:24 +0100

    conditionnelle
    conditionnelle

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,20 @@
     
     <span>Le compteur est à {{ count }}</span> 
     <div class="flex items-center">
+      <div v-if='count >= 0'>
       <button @click="increment" class="px-7 py-2 bg-green-950 text-green-300 rounded-md mr-2">Incrémenter</button>
-      <button @click="decrement" class="px-7 py-2 bg-green-950 text-green-300 rounded-md ml-2">Décrémenter</button>
+    </div>
+      
+        <button v-on:click="decrement" class="px-7 py-2 bg-green-950 text-green-300 rounded-md ml-2">Décrémenter</button>
+     
+
+    <div v-show='count >= 5' class="text-3xl ml-2 text-green-500 ">
+    Vous avez cliquez plus de  5 fois
+    </div>
+    <div v-show='count < 5' class="text-3xl ml-2 text-red-500 ">
+    Vous avez cliquez moins de 5 fois
+    </div>
+    
     </div>
  
 </template>
@@ -18,7 +30,9 @@ const count = ref(0)
 const name = ref("Rostand lele")
 
 
-const increment = () => {
+const increment = (event) => {
+  console.log(event);
+  
   count.value++
   
 }
