059dcb8fa2064b4e91388fa7582c6726381cad15
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-02 05:55:05 +0100

    dsqsqs
    dqdq

--- src/ListeText.vue
+++ src/ListeText.vue
@@ -12,6 +12,23 @@
     Reinitialiser
   </button>
 
+  <form action="" class="mt-2">
+    <input
+      type="text"
+      placeholder="New color"
+      class="border-3 border-x-black"
+      v-model="colorName"
+    />
+
+    {{ colorName}}
+    <button
+      @click="resetColors()"
+      class="bg-slate-600 p-2 rounded-md mr-3 text-white"
+    >
+      Ajouter
+    </button>
+  </form>
+
   <ul class="list-disc mt-4">
     <li v-for="color in colors" :key="color" class="flex items-center">
       <span>{{ color }}</span>
@@ -34,12 +51,24 @@
   </ul>
 
   <div v-if="isEditing" class="mt-4">
-      <h3 class="text-lg font-semibold">Modifier la couleur</h3>
-      <input v-model="colorToEdit" class="border border-gray-300 rounded px-2 py-1" />
-      <button @click="saveColor" class="bg-green-500 text-white px-4 py-2 rounded ml-3 hover:bg-green-600">Sauvegarder</button>
-      <button @click="closeEditDialog" class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 ml-2">Annuler</button>
-    </div>
- 
+    <h3 class="text-lg font-semibold">Modifier la couleur</h3>
+    <input
+      v-model="colorToEdit"
+      class="border border-gray-300 rounded px-2 py-1"
+    />
+    <button
+      @click="saveColor"
+      class="bg-green-500 text-white px-4 py-2 rounded ml-3 hover:bg-green-600"
+    >
+      Sauvegarder
+    </button>
+    <button
+      @click="closeEditDialog"
+      class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 ml-2"
+    >
+      Annuler
+    </button>
+  </div>
 </template>
 
 <script setup>
@@ -50,6 +79,7 @@ const colors = ref([...initialColors]); // Copie de l'état initial
 const colorIndex = ref(-1);
 const isEditing = ref(false);
 const colorToEdit = ref("");
+const colorName = ref("");
 
 const sortColors = () => {
   colors.value.sort((a, b) => (a > b ? 1 : -1));
@@ -75,7 +105,6 @@ const openEditDialog = (color) => {
 const saveColor = () => {
   if (colorIndex.value !== -1) {
     colors.value[colorIndex.value] = colorToEdit.value;
-    
   }
 };
 
@@ -84,5 +113,4 @@ const closeEditDialog = () => {
   isEditing.value = false;
   colorToEdit.value = "";
 };
-
 </script>
