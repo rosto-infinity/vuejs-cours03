46da109896e3380efcd53500ca38c385355a3093
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-22 16:14:32 +0100

    Propriété Dérivée
    Propriété Dérivée

--- src/TodoList.vue
+++ src/TodoList.vue
@@ -1,14 +1,21 @@
 <template>
   <form action="" @submit.prevent="addTodo">
-    <input type="text" v-model="newTodo" placeholder="Tâche à effectuer"
-      class="p-2 bg-green-50 focus:border-2 focus:border-red-500 border-l-2 border-b-2 border-t-2" />
+   
+   <input
+        type="text"
+        v-model="newTodo"
+        placeholder="Tâche à effectuer"
+        class="p-2 bg-green-50 focus:border-2 focus:border-red-500 border-l-2 border-b-2 border-t-2"
+      />
 
-
-    <button :disabled="newTodo.length === 0"
-      class="bg-green-500 text-white px-4 py-2 mr-5 border-green-500 border-r-2 border-b-2 border-t-2 rounded-r-md hover:bg-green-600 disabled:bg-green-300 disabled:border-green-300">
-      Ajouter une tâche
-    </button>
-
+      
+      <button
+        :disabled="newTodo.length === 0"
+        class="bg-green-500 text-white px-4 py-2 mr-5 border-green-500 border-r-2 border-b-2 border-t-2 rounded-r-md hover:bg-green-600 disabled:bg-green-300 disabled:border-green-300"
+      >
+        Ajouter une tâche
+      </button>
+    
   </form>
 
   <div v-if="todos.length === 0">
@@ -16,8 +23,13 @@
   </div>
   <div v-else>
     <ul>
-      <li class="list-disc ml-6 mt-2" v-for="todo in sortedTodos" :key="todo.date"
-        :class="{ 'line-through': todo.completed, 'opacity-50': todo.completed }">
+      <li
+        class="list-disc ml-6 mt-2"
+        
+        v-for="todo in sortedTodos"
+        :key="todo.date"
+        :class="{ 'line-through': todo.completed, 'opacity-50': todo.completed }"
+      >
         <input type="checkbox" v-model="todo.completed" /> {{ todo.title }}
         <button v-if="todo.completed" @click="deleteTodo(todo)" class="text-red-500 ml-2">Supprimer</button>
       </li>
@@ -47,21 +59,13 @@ const todos = ref([
 ]);
 
 const hideCompleted = ref(false); // Ajout de hideCompleted
-
-const sortedTodos = computed(() => {
-  const sortedList = todos.value.sort((a, b) => a.completed - b.completed);
-  return hideCompleted.value ? sortedList.filter(todo => !todo.completed) : sortedList;
-});
-
 const addTodo = () => {
-  if (newTodo.value.trim() !== '') {
     todos.value.push({
       title: newTodo.value,
       completed: false,
       date: Date.now(),
     });
     newTodo.value = '';
-  }
 };
 
 const sortTodos = () => {
@@ -71,4 +75,53 @@ const sortTodos = () => {
 const deleteTodo = (todo) => {
   todos.value = todos.value.filter(t => t !== todo);
 };
+
+// const sortedTodos = () => {
+//   console.log('list----');
+  
+//   // Trie d'abord la liste des tâches
+//   const sortedList = todos.value.toSorted((a, b) => a.completed - b.completed);
+
+//   // Déclaration de la variable pour stocker la liste filtrée
+//   let filteredList;
+
+//   // Utilisation de if et else pour filtrer les tâches
+//   if (hideCompleted.value) {
+//     // Si hideCompleted est vrai, filtre les tâches non complétées
+//     filteredList = sortedList.filter(todo => !todo.completed);
+//   } else {
+//     // Sinon, utilise la liste triée complète
+//     filteredList = sortedList;
+//   }
+
+//   // Retourne la liste filtrée
+//   return filteredList;
+// };
+
+// console.log(sortedTodos().value);
+// console.log(sortedTodos().value);
+// console.log(sortedTodos().value);
+// console.log(sortedTodos().value);
+
+// Propriété Dérivée
+
+const sortedTodos =computed(()=>{
+  console.log('demo');
+  
+  const sortedTodos =todos.value.toSorted((a,b) =>a.completed > b.completed ? 1 : -1 )
+
+  if (hideCompleted.value === true) {
+    return sortedTodos.filter(t => t.completed === false) 
+  }
+  return sortedTodos;
+});
+console.log(sortedTodos);
+console.log(sortedTodos);
+console.log(sortedTodos);
+console.log(sortedTodos);
+console.log(sortedTodos);
+
+
+
+
 </script>
